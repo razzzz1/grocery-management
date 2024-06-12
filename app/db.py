c797a1193492ba56398e6f88dca6ec4fea261afd
razzzz1 <ryanmaiyo1@gmail.com>
2024-06-12 14:56:27 +0300

    connection

---
+++ app/db.py
@@ -0,0 +1,11 @@
+# app/db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+
+class Database:
+    def __init__(self, db_uri):
+        self.engine = create_engine(db_uri)
+        Base.metadata.create_all(self.engine)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
